9b7203556da8867dc326ce279ec6703b89a0ccef
Uppala Sandeep Chowdary <sandeepchowdaryu@gmail.com>
2023-08-01 12:14:33 +0530

    Update spaRootBuild.groovy

--- vars/spaRootBuild.groovy
+++ vars/spaRootBuild.groovy
@@ -15,25 +15,20 @@ def call(Map config = [:]) {
         'epm-spa-dashboard' : "epm-dashboard",
     ]
     sh 'pwd'
+    def response = httpRequest authenticate: 'nexusrepositorycreds', "${nexusUrl}service/rest/v1/search?repository=ria-spa-repo&group=/epm-spa-integration&name=epm-spa-integration/epm-integration-2023.07*'"
+    echo ${response}
     withCredentials([usernamePassword(credentialsId: 'nexusrepositorycreds', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-      utitlities_url = """${nexusUrl}service/rest/v1/search?repository=ria-spa-repo&group=/epm-spa-utilities&name=epm-spa-utilities/epm-utilities-2023.07*"""
-      def utilities_version = sh(
-            returnStdout: true,
-            script: '''
-                curl -s -u ${USERNAME}:{PASSWORD} 'http://ec2-54-189-118-149.us-west-2.compute.amazonaws.com:8081/service/rest/v1/search?repository=ria-spa-repo&group=/epm-spa-integration&name=epm-spa-integration/epm-integration-2023.07*' | jq '.items[].assets[].path' | grep -oE '[0-9]+\\.[0-9]+\\.[0-9]+' | cut -d '.' -f 3 | sort -r | head -1
-            '''
-        ).trim()
       sh """
-      echo ${utilities_version}
+      
       //curl -u ${USERNAME}:${PASSWORD} ${nexusUrl}/epm-spa-utilities/epm-utilities-\$utilities_version.tar.gz -o epm-utilities.tar.gz;
-      mkdir -p epm-spa-utilities/dist;
+      //mkdir -p epm-spa-utilities/dist;
       //tar -xvzf epm-utilities.tar.gz -C epm-spa-utilities/dist;
       """
-      repo_map.each{k,v->
+      //repo_map.each{k,v->
         sh """
-        echo $component_version
+       // echo $component_version
         //curl -u ${USERNAME}:${PASSWORD} ${nexusUrl}/${k}/${v}-\$component_version.tar.gz -o ${v}.tar.gz;
-        mkdir -p ${k}/dist/release;
+        //mkdir -p ${k}/dist/release;
         //tar -xvzf ${v}.tar.gz -C ${k}/dist/release;
         """
       }
